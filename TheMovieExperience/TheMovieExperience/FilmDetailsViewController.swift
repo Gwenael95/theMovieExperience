f8987f0469382c18841f5bfece4fdc927faea480
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2021-03-24 16:36:54 +0100

    scroll view for details

---
+++ TheMovieExperience/TheMovieExperience/FilmDetailsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FilmDetailsViewController.swift
+//  TheMovieExperience
+//
+//  Created by GwenaelMarchetti on 24/03/2021.
+//
+
+import UIKit
+
+class FilmDetailsViewController: UIViewController {
+
+    var authors : [String] = ["Stanlee Kubric", "Steven Spielberg"]
+    var actors : [String] = ["Jean Dujardin", "Jack Nicholson", "Jean Reno"]
+    var filmName = "La derniere bataille"
+    var poster = ""
+    var date = "19 mai 1974"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

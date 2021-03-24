44d2ad286ae95375fb5e8c10fedf275e9c02d356
Hugo <hugo.monnerie@edu.itescia.fr>
2021-03-24 10:06:45 +0100

    refactor all

--- TheMovieExperience/TheMovieExperience/ViewController.swift
+++ TheMovieExperience/TheMovieExperience/ViewController.swift
@@ -1,20 +1,19 @@
 //
 //  ViewController.swift
-//  coursIos2
+//  TheMovieExperience
 //
-//  Created by GwenaelMarchetti on 22/03/2021.
+//  Created by Hugo Monnerie on 24/03/2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    
+
 }
 
